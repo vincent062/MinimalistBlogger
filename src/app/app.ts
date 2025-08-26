5602dea434e6714a598ae63a9bb21dbb9a735288
vincent <vince062580@gmail.com>
2025-08-26 14:32:43 +0200

    26/08

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import { Aside } from '../aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
